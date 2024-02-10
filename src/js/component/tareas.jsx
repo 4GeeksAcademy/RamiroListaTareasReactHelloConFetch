6f81f7e4e5ec6365d6e94da33d601f984f7a082e
Ramiro Scarinci <151871077+RamiroSca@users.noreply.github.com>
2024-02-10 13:02:09 +0000

    Correccion y arrego de detalles y css

--- src/js/component/tareas.jsx
+++ src/js/component/tareas.jsx
@@ -5,7 +5,7 @@ import React, {useState} from "react";
 const Tareas = () => {
     const [task,setTask]=useState("")
     const [list,setList]=useState([])
-    const [map,setMap]=useState([])
+    // const [map,setMap]=useState([]) !!!!IMPORTANTE MAP
 
     function addTask(e) {
         setTask(e.target.value)
@@ -14,15 +14,10 @@ const Tareas = () => {
     function sumbitTask(e) {
        
             if (e.key === "Enter") {
-            setList(list.concat(task))
-            setMap(list.map((i)=><li>{i}</li>))
+            setList(list.concat(<li>{task}</li>))
+            // setMap(list.map((i)=><li>{i}</li>)) !!!!IMPORTANTE MAP
             setTask("")
-            
-            // setMap(list.map((i)=><li>{i}</li>))
-            // list.map(setList(list),`<li>${list}</li></br>`)
-            // array.map(function(currentValue, index, arr), thisValue)
         }
-            // console.log(task,list)
     };
     
     
@@ -31,14 +26,14 @@ const Tareas = () => {
    
 
 	return (
-		<div>
-            <h1>TAREAS</h1>
-            <div>
-                <input onChange={addTask} value={task} onKeyDown={sumbitTask} ></input>
+		<div className="container">
+            <h1 className="mx-auto text-center">TAREAS</h1>
+            <div className="mx-auto text-center ">
+                <input className="bg-success p-2" onChange={addTask} value={task} onKeyDown={sumbitTask} ></input>
             </div>
-            <div>
-                <ul>
-                    {map}
+            <div className="mx-auto">
+                <ul className="mx-auto ">
+                    {list}
                 </ul>
             </div>
 		</div>
