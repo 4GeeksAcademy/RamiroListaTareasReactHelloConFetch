4243ad5b8d0a3c3d631bb164e5baf65daea40f1a
Ramiro Scarinci <151871077+RamiroSca@users.noreply.github.com>
2024-02-13 18:29:45 +0000

    Proyecto terminado con todas las funciones creadas

--- src/js/component/tareas.jsx
+++ src/js/component/tareas.jsx
@@ -1,19 +1,19 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 
 //create your first component
 const Tareas = () => {
-    const [task,setTask]=useState("")
-    const [list,setList]=useState([])
+    const [task, setTask] = useState("")
+    const [list, setList] = useState([])
     // const [map,setMap]=useState([]) !!!!IMPORTANTE MAP
 
     function addTask(e) {
         setTask(e.target.value)
-        }
+    }
 
     function sumbitTask(e) {
-       
-            if (e.key === "Enter") {
+
+        if (e.key === "Enter") {
             e.preventDefault();
             setList(list.concat([task]));
             // setList(list.concat({task}))
@@ -22,31 +22,33 @@ const Tareas = () => {
             setTask("")
         }
     };
-    
-    // const removeTask = (index) => {
-    //     setList(list.filter((_, i) => i !== index));
-    //   };
+
+    const removeTask = (index) => {
+        setList(list.filter((_, i) => i !== index));
+    };
     //   boton que va en la li de la task
     //   <button className="btn btn-danger ml-2" onClick={() => removeTask(index)}>
     //   X
     // </button>
-    
 
-   
 
-	return (
-		<div className="container">
+
+
+    return (
+        <div className="container">
             <h1 className="mx-auto text-center">TAREAS</h1>
             <div className="mx-auto text-center ">
                 <input className="bg-success p-2" onChange={addTask} value={task} onKeyDown={sumbitTask} ></input>
             </div>
             <div className="mx-auto">
-                <ul className="mx-auto ">
-                    {list.map((item, index) => (<li key={index}>{item}</li>))}
+                <ul className="mx-auto px-2">
+                    {list.map((item, index) => (<li key={index} className="d-flex justify-content-between">{item}<button className="btn btn-transparent text-danger ml-5" onClick={() => removeTask(index)}>
+                        X
+                    </button></li>))}
                 </ul>
             </div>
-		</div>
-	);
+        </div>
+    );
 };
 
 export default Tareas;
