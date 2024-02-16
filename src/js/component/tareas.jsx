e214daef6a31b478d7c3128046b5baa8b436c208
Ramiro Scarinci <151871077+RamiroSca@users.noreply.github.com>
2024-02-16 13:28:59 +0000

    avanzando con el sistema de registro

--- src/js/component/tareas.jsx
+++ src/js/component/tareas.jsx
@@ -1,54 +1,81 @@
-import React, { useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 
 
 //create your first component
 const Tareas = () => {
     const [task, setTask] = useState("")
     const [list, setList] = useState([])
-    const [inicio, setInicio] = useState("")
-    const [nameRegistro, setNameRegistro] = useState("")
+    // const [inicio, setInicio] = useState("")
+    // const [nameRegistro, setNameRegistro] = useState("")
     // const [map,setMap]=useState([]) !!!!IMPORTANTE MAP
 
 
 
-function Onload () {
-    let consulta = prompt(`marque 1 si quiere iniciar sesión o marque 2 si quiere registrarse`) 
-    if (consulta==1) {
-        setInicio(prompt(`Para iniciar sesión: indique su nombre de usuario`))
-        getIniciar(inicio)
-        // setNameRegistro(inicio)
-    } else {
-        setNameRegistro(prompt(`Para Registrarse: indique su nombre de usuario`))
-        postRegistrar(nameRegistro)
-        getIniciar(nameRegistro)
-        // setInicio(nameRegistro)
+    function Onload() {
+        let consulta = prompt(`marque 1 si quiere iniciar sesión o marque 2 si quiere registrarse`)
+        if (consulta == 1) {
+            let inicio = prompt(`Para iniciar sesión: indique su nombre de usuario`)
+            // console.log(inicio)
+            getIniciar(inicio)
+            // setNameRegistro(inicio)
+        } else {
+            let nameRegistro = prompt(`Para Registrarse: indique su nombre de usuario`)
+            // console.log(nameRegistro);
+            postRegistrar(nameRegistro)
 
+            // setInicio(nameRegistro)
+
+        }
     }
-}
 
 
 
 
-function postRegistrar(name) {
+    function postRegistrar(name) {
 
-        const data = [];
-    
+        // const data = [];
+
         fetch(`https://playground.4geeks.com/apis/fake/todos/user/${name}`, {
             method: "POST",
-            body: JSON.stringify(data),
+            body: JSON.stringify([]),
             headers: {
                 "Content-Type": "application/json"
             }
         })
+            .then(resp => {
+                console.log(resp.ok); // Será true si la respuesta es exitosa
+                console.log(resp.status); // El código de estado 200, 300, 400, etc.
+                // console.log(resp.text()); // Intentará devolver el resultado exacto como string
+                return resp.json();
+            })
+            .then(data => {
+                // Aquí es donde debe comenzar tu código después de que finalice la búsqueda
+                console.log(data);// Esto imprimirá en la consola el objeto exacto recibido del servidor
+                getIniciar(name)                
+            })
+            .catch(error => {
+                // Manejo de errores
+                console.log(error);
+            });
+
+
+    }
+
+    function getIniciar(name) {
+
+        fetch(`https://playground.4geeks.com/apis/fake/todos/user/${name}`)
         .then(resp => {
             console.log(resp.ok); // Será true si la respuesta es exitosa
             console.log(resp.status); // El código de estado 200, 300, 400, etc.
-            console.log(resp.text()); // Intentará devolver el resultado exacto como string
-            return resp.json();
+            // console.log(resp.text()); // Intentará devolver el resultado exacto como string
+            return resp.json(); // Intentará parsear el resultado a JSON y retornará una promesa donde puedes usar .then para seguir con la lógica
         })
         .then(data => {
             // Aquí es donde debe comenzar tu código después de que finalice la búsqueda
-            console.log(data); // Esto imprimirá en la consola el objeto exacto recibido del servidor
+            setList(data); // Esto imprimirá en la consola el objeto exacto recibido del servidor
+            
+            console.log(data)
+            console.log(list);
         })
         .catch(error => {
             // Manejo de errores
@@ -56,51 +83,33 @@ function postRegistrar(name) {
         });
     }
 
-function getIniciar(name) {
-    fetch(`https://playground.4geeks.com/apis/fake/todos/user/${name}`)
-    .then(resp => {
-        console.log(resp.ok); // Será true si la respuesta es exitosa
-        console.log(resp.status); // El código de estado 200, 300, 400, etc.
-        console.log(resp.text()); // Intentará devolver el resultado exacto como string
-        return resp.json(); // Intentará parsear el resultado a JSON y retornará una promesa donde puedes usar .then para seguir con la lógica
-    })
-    .then(data => {
-        // Aquí es donde debe comenzar tu código después de que finalice la búsqueda
-        setList(data); // Esto imprimirá en la consola el objeto exacto recibido del servidor
-    })
-    .catch(error => {
-        // Manejo de errores
-        console.log(error);
-    });
-}
+    // function putGuardar(ListaDeTareas) {
 
-// function putGuardar(ListaDeTareas) {
+    //     const data = ListaDeTareas;
 
-//     const data = ListaDeTareas;
-    
-//         fetch(`https://playground.4geeks.com/apis/fake/todos/user/${inicio}`, {
-//             method: "PUT",
-//             body: JSON.stringify(data),
-//             headers: {
-//                 "Content-Type": "application/json"
-//             }
-//         })
-//         .then(resp => {
-//             console.log(resp.ok); // Será true si la respuesta es exitosa
-//             console.log(resp.status); // El código de estado 200, 300, 400, etc.
-//             console.log(resp.text()); // Intentará devolver el resultado exacto como string
-//             return resp.json();
-//         })
-//         .then(data => {
-//             // Aquí es donde debe comenzar tu código después de que finalice la búsqueda
-//             console.log(data); // Esto imprimirá en la consola el objeto exacto recibido del servidor
-//         })
-//         .catch(error => {
-//             // Manejo de errores
-//             console.log(error);
-//         });
-    
-// }
+    //         fetch(`https://playground.4geeks.com/apis/fake/todos/user/${inicio}`, {
+    //             method: "PUT",
+    //             body: JSON.stringify(data),
+    //             headers: {
+    //                 "Content-Type": "application/json"
+    //             }
+    //         })
+    //         .then(resp => {
+    //             console.log(resp.ok); // Será true si la respuesta es exitosa
+    //             console.log(resp.status); // El código de estado 200, 300, 400, etc.
+    //             console.log(resp.text()); // Intentará devolver el resultado exacto como string
+    //             return resp.json();
+    //         })
+    //         .then(data => {
+    //             // Aquí es donde debe comenzar tu código después de que finalice la búsqueda
+    //             console.log(data); // Esto imprimirá en la consola el objeto exacto recibido del servidor
+    //         })
+    //         .catch(error => {
+    //             // Manejo de errores
+    //             console.log(error);
+    //         });
+
+    // }
 
 
     function addTask(e) {
@@ -111,7 +120,8 @@ function getIniciar(name) {
 
         if (e.key === "Enter") {
             e.preventDefault();
-            setList(list.concat({ label:task, done: false }));
+            // setList(list.concat({ label:task, done: false }));
+            setList(list.concat({ label: task, done: false }))
             // putGuardar(list)
             // setList(list.concat({task}))
             // setMap(list.map((i)=><li>{i}</li>)) !!!!IMPORTANTE MAP
@@ -121,7 +131,7 @@ function getIniciar(name) {
     };
 
     const removeTask = (index) => {
-        setList(list.filter((_, i) => i !== index));
+        setList(list.filter((item, i) => i !== index));
         console.log(index);
     };
     //   boton que va en la li de la task
@@ -129,12 +139,12 @@ function getIniciar(name) {
     //   X
     // </button>
 
-useEffect(()=>{
-// getArr()
-// setArr(prompt(`Nombre de Usuario`))
-Onload()
-// postRegistrar("JohnDoe");
-},[])
+    useEffect(() => {
+        // getArr()
+        // setArr(prompt(`Nombre de Usuario`))
+        Onload()
+        // postRegistrar("JohnDoe");
+    }, [])
 
 
 
@@ -146,9 +156,10 @@ Onload()
             </div>
             <div className="mx-auto">
                 <ul className="mx-auto px-2">
-                    {list.map((item, index) => (<li id="oculto" key={index} className="d-flex justify-content-between">{item.label}<button className="btn btn-transparent text-danger ml-5 oculto" onClick={() => removeTask(index)}>
-                        X
-                    </button></li>))}
+                    
+                    {/* {list.map((item, index) => (<li id="oculto" key={index} className="d-flex justify-content-between">{item[index].label}<button className="btn btn-transparent text-danger ml-5 oculto" onClick={() => removeTask(index)}>
+                    X
+                    </button></li>))} */}
                     <li className="tareasPorHacer d-flex">{list.length} tareas por Hacer</li>
                 </ul>
             </div>
@@ -157,3 +168,4 @@ Onload()
 };
 
 export default Tareas;
+
