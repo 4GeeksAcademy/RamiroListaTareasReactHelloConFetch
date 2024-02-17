5ff64a19db0b00b149deb6dc567343a766fed979
Ramiro Scarinci <151871077+RamiroSca@users.noreply.github.com>
2024-02-17 13:27:12 +0000

    Creacion del modal y acomodandolo

--- src/js/component/tareas.jsx
+++ src/js/component/tareas.jsx
@@ -6,29 +6,33 @@ const Tareas = () => {
     const [task, setTask] = useState("")
     const [list, setList] = useState([])
     // const [inicio, setInicio] = useState("")
+    const [inputValue, setInputValue] = useState("");
     // const [nameRegistro, setNameRegistro] = useState("")
     // const [map,setMap]=useState([]) !!!!IMPORTANTE MAP
 
 
 
-    function Onload() {
-        let consulta = prompt(`marque 1 si quiere iniciar sesión o marque 2 si quiere registrarse`)
-        if (consulta == 1) {
-            let inicio = prompt(`Para iniciar sesión: indique su nombre de usuario`)
-            // console.log(inicio)
-            getIniciar(inicio)
-            // setNameRegistro(inicio)
-        } else {
-            let nameRegistro = prompt(`Para Registrarse: indique su nombre de usuario`)
-            // console.log(nameRegistro);
-            postRegistrar(nameRegistro)
+    // function Onload() {
+    //     let consulta = prompt(`marque 1 si quiere iniciar sesión o marque 2 si quiere registrarse`)
+    //     if (consulta == 1) {
+    //         let userName = prompt(`Para iniciar sesión: indique su nombre de usuario`)
+    //         setInicio(userName)
+    //         getIniciar(userName)
+    //         setNameRegistro(userName)
+    //     } else {
+    //         let userNameRegistro = prompt(`Para Registrarse: indique su nombre de usuario`)
+    //         setNameRegistro(userNameRegistro)
+    //         postRegistrar(userNameRegistro)
+    //         setInicio(userNameRegistro)
 
-            // setInicio(nameRegistro)
+    //     }
+    // }
 
-        }
-    }
-
-
+    const onFormSumbit = (event) => {
+        event.preventDefault();
+        getIniciar(inputValue)
+        return(inputValue);
+    };
 
 
     function postRegistrar(name) {
@@ -51,7 +55,7 @@ const Tareas = () => {
             .then(data => {
                 // Aquí es donde debe comenzar tu código después de que finalice la búsqueda
                 console.log(data);// Esto imprimirá en la consola el objeto exacto recibido del servidor
-                getIniciar(name)                
+                getIniciar(name)
             })
             .catch(error => {
                 // Manejo de errores
@@ -62,54 +66,76 @@ const Tareas = () => {
     }
 
     function getIniciar(name) {
-
+    
         fetch(`https://playground.4geeks.com/apis/fake/todos/user/${name}`)
-        .then(resp => {
-            console.log(resp.ok); // Será true si la respuesta es exitosa
-            console.log(resp.status); // El código de estado 200, 300, 400, etc.
-            // console.log(resp.text()); // Intentará devolver el resultado exacto como string
-            return resp.json(); // Intentará parsear el resultado a JSON y retornará una promesa donde puedes usar .then para seguir con la lógica
-        })
-        .then(data => {
-            // Aquí es donde debe comenzar tu código después de que finalice la búsqueda
-            setList(data); // Esto imprimirá en la consola el objeto exacto recibido del servidor
-            
-            console.log(data)
-            console.log(list);
-        })
-        .catch(error => {
-            // Manejo de errores
-            console.log(error);
-        });
-    }
+          .then((resp) => {
+            if (!resp.ok) {
+              throw new Error("Error en la respuesta del servidor");
+            }
+            return resp.json();
+          })
+          .then((data) => {
+            setList(data);
+          })
+          .catch((error) => {
+            if (error.name === "AbortError") {
+              console.log("La petición ha sido cancelada");
+            } else {
+              console.log("Hubo un error en la petición:", error);
+            }
+          });}
 
-    // function putGuardar(ListaDeTareas) {
+    // function getIniciar(name) {
 
-    //     const data = ListaDeTareas;
-
-    //         fetch(`https://playground.4geeks.com/apis/fake/todos/user/${inicio}`, {
-    //             method: "PUT",
-    //             body: JSON.stringify(data),
-    //             headers: {
-    //                 "Content-Type": "application/json"
-    //             }
-    //         })
+    //     fetch(`https://playground.4geeks.com/apis/fake/todos/user/${name}`)
     //         .then(resp => {
     //             console.log(resp.ok); // Será true si la respuesta es exitosa
     //             console.log(resp.status); // El código de estado 200, 300, 400, etc.
-    //             console.log(resp.text()); // Intentará devolver el resultado exacto como string
-    //             return resp.json();
+    //             if (resp.status == 404) {
+    //                 postRegistrar(name)
+    //             }
+    //             // console.log(resp.text()); // Intentará devolver el resultado exacto como string
+    //             return resp.json(); // Intentará parsear el resultado a JSON y retornará una promesa donde puedes usar .then para seguir con la lógica
     //         })
     //         .then(data => {
     //             // Aquí es donde debe comenzar tu código después de que finalice la búsqueda
-    //             console.log(data); // Esto imprimirá en la consola el objeto exacto recibido del servidor
+    //             setList(data);
+                
+    //              // Esto imprimirá en la consola el objeto exacto recibido del servidor
+
+    //             console.log(data)
+    //             console.log(list);
     //         })
     //         .catch(error => {
     //             // Manejo de errores
     //             console.log(error);
     //         });
-
     // }
+
+    function putGuardar(array) {
+        fetch(`https://playground.4geeks.com/apis/fake/todos/user/${inputValue}`, {
+            method: "PUT",
+            body: JSON.stringify(array),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+            .then(resp => {
+                console.log(resp.ok); // Será true si la respuesta es exitosa
+                console.log(resp.status); // El código de estado 200, 300, 400, etc.
+                // console.log(resp.text()); // Intentará devolver el resultado exacto como string
+                return resp.json();
+            })
+            .then(data => {
+                // Aquí es donde debe comenzar tu código después de que finalice la búsqueda
+                console.log(data); // Esto imprimirá en la consola el objeto exacto recibido del servidor
+            })
+            .catch(error => {
+                // Manejo de errores
+                console.log(error);
+            });
+
+    }
 
 
     function addTask(e) {
@@ -127,39 +153,70 @@ const Tareas = () => {
             // setMap(list.map((i)=><li>{i}</li>)) !!!!IMPORTANTE MAP
             // setList(list.concat(<li>{task}</li>))
             setTask("")
+            putGuardar(list)
         }
     };
 
     const removeTask = (index) => {
         setList(list.filter((item, i) => i !== index));
         console.log(index);
+        putGuardar(list)
     };
     //   boton que va en la li de la task
     //   <button className="btn btn-danger ml-2" onClick={() => removeTask(index)}>
     //   X
     // </button>
 
-    useEffect(() => {
-        // getArr()
-        // setArr(prompt(`Nombre de Usuario`))
-        Onload()
-        // postRegistrar("JohnDoe");
-    }, [])
+
+    // useEffect(() => {
+        
+    //     // getArr()
+    //     // setArr(prompt(`Nombre de Usuario`))
+    //     // Onload()
+    //     // postRegistrar("JohnDoe");
+    // }, [])
 
 
 
     return (
         <div className="container">
-            <h1 className="mx-auto text-center">TAREAS</h1>
+            <div>
+                {/* <!-- Button trigger modal --> */}
+                <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                    Iniciar Sesión / Registrarse
+                </button>
+
+                {/* <!-- Modal --> */}
+                <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div className="modal-dialog">
+                        <div className="modal-content">
+                            <form onSubmit={onFormSumbit}>
+                                <div className="modal-header">
+                                    <h1 className="modal-title fs-5" id="exampleModalLabel">Ingrese su nombre de usuario</h1>
+                                    <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <div className="modal-body">
+                                    <input type="text" name="name" className="" value={inputValue} onChange={(e) => setInputValue(e.target.value)}></input>
+                                    <button type="submit" className="btn btn-primary" data-bs-dismiss="modal">Save changes</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+
+
+
+
+                <h1 className="mx-auto text-center">TAREAS</h1>
+            </div>
             <div className="mx-auto text-center ">
                 <input className="bg-success p-2" onChange={addTask} value={task} onKeyDown={sumbitTask} ></input>
             </div>
             <div className="mx-auto">
                 <ul className="mx-auto px-2">
-                    
-                    {/* {list.map((item, index) => (<li id="oculto" key={index} className="d-flex justify-content-between">{item[index].label}<button className="btn btn-transparent text-danger ml-5 oculto" onClick={() => removeTask(index)}>
-                    X
-                    </button></li>))} */}
+                    {list.map((item, index) => (<li id="oculto" key={index} className="d-flex justify-content-between">{item.label}<button className="btn btn-transparent text-danger ml-5 oculto" onClick={() => removeTask(index)}>
+                        X
+                    </button></li>))}
                     <li className="tareasPorHacer d-flex">{list.length} tareas por Hacer</li>
                 </ul>
             </div>
